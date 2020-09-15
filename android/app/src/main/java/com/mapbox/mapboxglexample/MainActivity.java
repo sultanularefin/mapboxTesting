f0b4ca55b9868b6d69a9087f245d798311ad7077
sultanularefin <mhmdarefin@gmail.com>
2020-09-15 08:42:31 +0600

    commit before checkout to migration work branch

--- android/app/src/main/java/com/mapbox/mapboxglexample/MainActivity.java
+++ android/app/src/main/java/com/mapbox/mapboxglexample/MainActivity.java
@@ -1,4 +1,5 @@
-package com.mapbox.mapboxglexample;
+// iteration 1..
+/*package com.mapbox.mapboxglexample;
 
 import android.os.Bundle;
 import io.flutter.app.FlutterActivity;
@@ -9,5 +10,44 @@ public class MainActivity extends FlutterActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     GeneratedPluginRegistrant.registerWith(this);
+  }
+}
+
+*/
+
+
+// MainActivity.java
+//-import android.os.Bundle;
+//-import io.flutter.app.FlutterActivity;
+import io.flutter.embedding.android.FlutterActivity;
+//-import io.flutter.plugins.GeneratedPluginRegistrant;
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugin.common.MethodChannel;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+
+
+public class MainActivity extends FlutterActivity {
+
+  private static final String CHANNEL = "samples.flutter.dev/battery";
+//-  @Override
+//-  protected void onCreate(Bundle savedInstanceState) {
+//    -    super.onCreate(savedInstanceState);
+//    -    GeneratedPluginRegistrant.registerWith(this);
+//    -  }
+
+
+  @Override
+  public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+    GeneratedPluginRegistrant.registerWith(flutterEngine);
+    new MethodChannel(flutterEngine.getDartExecutor().getBinaryMessenger(), CHANNEL)
+            .setMethodCallHandler(
+                    (call, result) -> {
+                      // Your existing code
+                    }
+            );
   }
 }
--- android/app/src/main/kotlin/com/mapbox/example/MainActivity.kt
+++ android/app/src/main/kotlin/com/mapbox/example/MainActivity.kt
@@ -1,4 +1,5 @@
-package com.mapbox.example
+//## iteration 1
+/*package com.mapbox.example
 
 import androidx.annotation.NonNull;
 import io.flutter.embedding.android.FlutterActivity
@@ -8,5 +9,35 @@ import io.flutter.plugins.GeneratedPluginRegistrant
 class MainActivity: FlutterActivity() {
     override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
         GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
+*/
+
+
+package com.mapbox.example
+// MainActivity.kt
+//-import android.os.Bundle
+//-import io.flutter.app.FlutterActivity
++import io.flutter.embedding.android.FlutterActivity
+//-import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+//    -  override fun onCreate(savedInstanceState: Bundle?) {
+//        -    super.onCreate(savedInstanceState)
+//        -    GeneratedPluginRegistrant.registerWith(this)
+//        -  }
+
+    //-  @Override
+    //-  protected void onCreate(Bundle savedInstanceState) {
+    //    -    super.onCreate(savedInstanceState);
+    //    -    GeneratedPluginRegistrant.registerWith(this);
+    //    -  }
+    @Override
+    fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine)
+        MethodChannel(flutterEngine.getDartExecutor().getBinaryMessenger(), MainActivity.CHANNEL)
+                .setMethodCallHandler(
+                        { call, result -> }
+                )
     }
 }
